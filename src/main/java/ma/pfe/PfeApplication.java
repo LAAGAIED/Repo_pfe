6bd4760920f2e3ce70a110cc54e1e6233a70538b
LAAGAIED <LAAGAIED@GMAIL.COM>
2023-03-19 11:24:00 +0000

    "connectionMysql"

--- src/main/java/ma/pfe/PfeApplication.java
+++ src/main/java/ma/pfe/PfeApplication.java
@@ -1,11 +1,23 @@
 package ma.pfe;
 
+
+import org.hibernate.cfg.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class PfeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(PfeApplication.class);
+    private  final static Logger LOGGER= LoggerFactory.getLogger(PfeApplication.class);
+
+    public static void main(String[] args)
+    {
+        SpringApplication app=new SpringApplication(PfeApplication.class);
+        ConfigurableEnvironment environment=app.run(args).getEnvironment();
+        LOGGER.info("Access URLs:\n-------------------------------------------------------\n" +
+                "Local: \t\thttp://localhost:{}/swagger-ui/\n\t" ,environment.getProperty("server.port"));
     }
 }
