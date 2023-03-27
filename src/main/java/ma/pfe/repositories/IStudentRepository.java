17000437bd408764cb50bd029c024a7a9eef271d
LAAGAIED <LAAGAIED@GMAIL.COM>
2023-03-27 11:50:38 +0000

    log ok

--- src/main/java/ma/pfe/repositories/IStudentRepository.java
+++ src/main/java/ma/pfe/repositories/IStudentRepository.java
@@ -2,10 +2,9 @@ package ma.pfe.repositories;
 
 import ma.pfe.entities.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Repository("Repo1")
 public interface IStudentRepository extends JpaRepository<StudentEntity,Long> {
